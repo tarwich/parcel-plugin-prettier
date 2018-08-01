1878e170dfec74f19ae9179e65229654a1109c00
Samuel Dillow <tarwich@gmail.com>
2018-08-01 16:29:40 -0500

    Added trailing comma

--- prettify.js
+++ prettify.js
@@ -6,6 +6,7 @@ const prettifyFile = filepath => {
   const newCode = format(code, {
     filepath,
     singleQuote: true,
+    trailingComma: true,
   });
 
   if (newCode !== code) writeFileSync(filepath, newCode);
