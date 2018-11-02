5e94d73a036b59833ba47543ea6caae72376dbd9
Samuel Dillow <tarwich@gmail.com>
2018-11-02 13:17:48 -0500

    I am confused. Adding JSX back as it seems to be working now

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = bundler => {
   bundler.addAssetType('tsx', require.resolve('./prettier-typescript'));
   bundler.addAssetType('ts', require.resolve('./prettier-typescript'));
+  bundler.addAssetType('jsx', require.resolve('./prettier-javascript'));
   bundler.addAssetType('js', require.resolve('./prettier-javascript'));
   bundler.addAssetType('scss', require.resolve('./prettier-sass'));
 };
