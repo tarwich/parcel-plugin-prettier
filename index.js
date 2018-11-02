d9893647b6f6a811059890bd27e40adba901bf58
Samuel Dillow <tarwich@gmail.com>
2018-11-02 11:51:03 -0500

    Added js and jsx to the files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = bundler => {
   bundler.addAssetType('tsx', require.resolve('./prettier-typescript'));
   bundler.addAssetType('ts', require.resolve('./prettier-typescript'));
+  bundler.addAssetType('jsx', require.resolve('./prettier-typescript'));
+  bundler.addAssetType('js', require.resolve('./prettier-typescript'));
   bundler.addAssetType('scss', require.resolve('./prettier-sass'));
 };
